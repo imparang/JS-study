bb95b8ce00a99b95cac92455bf9a70ac441b0383
imparang <jeongjaeyn@gmail.com>
2021-10-21 21:47:05 +0900

    feat: Import and export js files

---
+++ 3/3-8.js
@@ -0,0 +1,16 @@
+import { checkType } from "../utils/index.js";
+
+// ! Import
+//* import @ from ...
+//* 1. 기본 내보내기한 것을 받기
+
+//* import {@} from ...
+//* 2. 이름을 가진 내보내기한 것을 받기
+//* import * as @ from ...
+//* @이라는 모듈로 다 가져옴
+//* import {@ as &}from ...
+//? 결국 as는 이름을 바꿔준다.
+
+// console.log(checkType(123, "Number"));
+// console.log(random(4, 7));
+console.log(checkType);
